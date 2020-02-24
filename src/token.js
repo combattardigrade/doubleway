051045ce9348dcfd4c1cb6781f340d812fc75a90
combattardigrade <combattardigrade@gmail.com>
2020-02-23 18:43:14 -0600

    login/signup system

--- src/token.js
+++ src/token.js
@@ -1,7 +1,7 @@
 import Web3 from 'web3'
 
 const ABI = [{ "constant": true, "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "name": "userList", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "users", "outputs": [{ "internalType": "bool", "name": "isExist", "type": "bool" }, { "internalType": "uint256", "name": "id", "type": "uint256" }, { "internalType": "uint256", "name": "referrerID", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "internalType": "address", "name": "_user", "type": "address" }, { "internalType": "uint256", "name": "_level", "type": "uint256" }], "name": "viewUserLevelExpired", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }]
-const CONTRACT = '0x45266CD8e2bd4F25a5DDa555C86244503768f22d'
+const CONTRACT = '0x41758254951f425fe0678555E4E6c2f226fC2012'
 const HTTPPROVIDER = 'https://ropsten.infura.io/v3/9bae6681948f47aca03246c6d6b7b8e4'
 
 const App = {
@@ -19,7 +19,26 @@ const App = {
             await App.startContract()
 
             // Check if user exists in contract
-            await this.checkUserExists()
+            const userExists = await this.checkUserExists()
+            if (userExists) {
+                window.location.replace('/dashboard')
+                return
+            }
+        }
+    },
+
+    manualLogin: async function () {
+        // Connect to HTTP Provider
+        this.web3 = new Web3(new Web3.providers.HttpProvider(HTTPPROVIDER))
+        // Get account input
+        App.account = document.getElementById('manualLoginAccount').value
+        // Start contract
+        await App.startContract()
+        // Check if user exists in contract
+        const userExists = await this.checkUserExists()
+        if (userExists) {
+            window.location.replace('/dashboard')
+            return
         }
     },
 
@@ -41,7 +60,9 @@ const App = {
         const { users } = this.meta.methods
         let response = await users(this.account).call()
         if (!response.isExist) {
+            // document.getElementById('networkResponse').innerHTML = 'La cuenta no existe por favor registra una cuenta nueva para ingresar. Serás redireccionado a la página de registro en 5 segundos'
             window.location.replace('/signup')
+            return
         }
         // save user in localStorage
         const user = {
@@ -52,11 +73,38 @@ const App = {
         }
         localStorage.setItem('user', JSON.stringify(user))
         console.log(localStorage.getItem('user'))
+        return true
     },
 
     signup: async function () {
-        await App.connectHttpProvider()
-        await App.startContract()
+        if (window.ethereum) {
+            this.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+            const accounts = await this.web3.eth.getAccounts()
+            App.account = accounts[0]
+            // Start contract
+            await App.startContract()
+
+            // Get upline address
+            const uplineUser = JSON.parse(localStorage.getItem('uplineUser'))
+
+            // Send transaction
+            const tx = {
+                from: App.account,
+                to: CONTRACT,
+                value: this.web3.utils.toWei("0.08", "ether"), // level1 price
+                data: uplineUser.address, // upline      
+                gas: 2000000
+            }
+            this.web3.eth.sendTransaction(tx, function (err, res) {
+                if (err) {
+                    console.log(err)
+                    return
+                }
+                console.log(res)
+                // redirect or show message
+            })
+        }
     },
 
     checkUpline: async function (upline) {
@@ -74,18 +122,18 @@ const App = {
             // Get upline address with ID
             const uplineAddress = await contract.methods.userList(upline).call()
             // Check if ID exists = if returned address is valid
-            if(uplineAddress == '0x0000000000000000000000000000000000000000') {
-                return { value: false, msg: 'No se encontró el miembro con la identificación especificada o la dirección ethereum'}
+            if (uplineAddress == '0x0000000000000000000000000000000000000000') {
+                return { value: false, msg: 'No se encontró el miembro con la identificación especificada o la dirección ethereum' }
             }
             // Save uplineUser in localstorage
-            localStorage.setItem('uplineUser', JSON.stringify({address: uplineAddress, id: upline, isExist: true}))
+            localStorage.setItem('uplineUser', JSON.stringify({ address: uplineAddress, id: upline, isExist: true }))
             console.log(localStorage.getItem('uplineUser'))
             return { value: true, address: uplineAddress }
         }
 
         // Check if user exists with address
         const response = await contract.methods.users(upline).call()
-        
+
         if (!response.isExist) {
             return { value: false, msg: 'No se encontró el miembro con la identificación especificada o la dirección ethereum' }
         }
@@ -115,32 +163,32 @@ const App = {
         }
 
         // Check upline 
-        const response = await App.checkUpline(upline)        
+        const response = await App.checkUpline(upline)
 
         // If upline is not valid show error message
-        if(!response.value) {
+        if (!response.value) {
             document.getElementById('networkResponse').innerHTML = response.msg
             return
         }
 
         // Continue witih signup process
-        document.getElementById('signupForm-2').style.display='block'
-        document.getElementById('signupForm-1').style.display='none'
+        document.getElementById('signupForm-2').style.display = 'block'
+        document.getElementById('signupForm-1').style.display = 'none'
 
     },
 
-    getUpline: async function() {
+    getUpline: async function () {
         const uplineUser = {
-            address: '0x417aA69d45D64eEb351672Fa8AF8f02cdcB3CE87',
+            address: '0xA578585b12949a9D41bA10B159Bc5089a26DdD75',
             isExist: true,
             id: 1,
             referrerID: 0
         }
         localStorage.setItem('uplineUser', JSON.stringify(uplineUser))
         console.log(localStorage.getItem('uplineUser'))
-        document.getElementById('signupForm-2').style.display='block'
-        document.getElementById('signupForm-1').style.display='none'
-        return 
+        document.getElementById('signupForm-2').style.display = 'block'
+        document.getElementById('signupForm-1').style.display = 'none'
+        return
     },
 
     connectHttpProvider: async function () {
@@ -155,9 +203,10 @@ window.App = App
 window.addEventListener("load", async function () {
     const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/9bae6681948f47aca03246c6d6b7b8e4'))
     const contract = new web3.eth.Contract(ABI, CONTRACT)
-    // const response = await contract.methods.userList('1').call()
+    console.log(contract.methods)
+    //await contract({from: web3.eth.accounts[0], gas: 400000, value: 0.08})
 
-    
+
 });
 
 // const autoLoginBtn = document.getElementById('auto-login-btn')
